6ab8d1af2415eebf78beccc1cbfa998d56b52030
Imad Labrini <imadlabrini16@gmail.com>
2024-08-02 19:03:47 +0100

    ff

--- SONAR Ecomerce App/lib/Screens/cart/widgets/checkout.dart
+++ SONAR Ecomerce App/lib/Screens/cart/widgets/checkout.dart
@@ -11,7 +11,7 @@ class CheckoutCard extends StatelessWidget {
   Widget build(BuildContext context) {
     // ProductController prodCon = Get.find<ProductController>();
     return Container(
-      height: 140,
+      height: 130,
       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
       decoration: BoxDecoration(
         color: Colors.deepPurple[300],
